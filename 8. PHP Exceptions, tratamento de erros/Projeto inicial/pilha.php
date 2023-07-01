19d796b9f996a9cd239891d72fa41fafc6c5ffdb
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-07-01 16:41:10 -0300

    curso de exceptions

---
+++ 8. PHP Exceptions, tratamento de erros/Projeto inicial/pilha.php
@@ -0,0 +1,26 @@
+<?php
+
+function funcao1()
+{
+    echo 'Entrei na função 1' . PHP_EOL;
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $erro) {
+        echo $erro->getMessage() . PHP_EOL;
+        echo $erro->getLine() . PHP_EOL;
+        echo $erro->getTraceAsString() . PHP_EOL;
+    }
+    echo 'Saindo da função 1' . PHP_EOL;
+}
+
+function funcao2()
+{
+    echo 'Entrei na função 2' . PHP_EOL;
+    $exception = new RuntimeException();
+    throw $exception;
+    echo 'Saindo da função 2' . PHP_EOL;
+}
+
+echo 'Iniciando o programa principal' . PHP_EOL;
+funcao1();
+echo 'Finalizando o programa principal' . PHP_EOL;
